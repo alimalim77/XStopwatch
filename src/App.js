76961152809e3b0da61f552efdee6b5399855323
alimalim77 <alimalim77@gmail.com>
2024-03-06 01:32:05 +0530

    Final Changes

--- src/App.js
+++ src/App.js
@@ -32,9 +32,12 @@ function App() {
   };
 
   const formatted = () => {
-    let minutes = Number(curTime) / 60;
+    let minutes = Math.floor(Number(curTime) / 60);
     let seconds = Number(curTime) % 60;
-    return `${Math.floor(minutes)}:${Math.floor(seconds)}`; //60 + ' ' +;
+    // if (String(minutes).length < 2) minutes = "0" + String(minutes);
+    if (String(seconds).length < 2) seconds = "0" + String(seconds);
+
+    return `${minutes}:${seconds}`; //60 + ' ' +;
   };
 
   return (
