70c53f884feb84fd200aae9bfc37c99190367aaf
alimalim77 <alimalim77@gmail.com>
2024-03-06 00:30:39 +0530

    DOne

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [curTime, setCurTime] = useState(0);
+  const [toggle, setToggle] = useState("Start");
+  const [reset, setReset] = useState(false);
+
+  const [intervalId, setIntervalId] = useState(null);
+
+  const handleWatch = (id) => {
+    toggle === "Start" ? setToggle("Stop") : setToggle("Start");
+    if (!intervalId) {
+      const timer = setInterval(() => {
+        setCurTime((prev) => prev + 1);
+      }, 1000);
+      setIntervalId(timer);
+    } else {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+  };
+
+  const handleReset = (id) => {
+    setCurTime(0);
+    if (id) {
+      clearInterval(id);
+      setIntervalId(null);
+    }
+  };
+
+  const formatted = () => {
+    let minutes = Number(curTime) / 60;
+    let seconds = Number(curTime) % 60;
+    return `${Math.floor(minutes)}:${Math.floor(seconds)}`; //60 + ' ' +;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stopwatch</h1>
+      <div className="time">Time: {formatted()}</div>
+      <div>
+        <button type="button" onClick={() => handleWatch(intervalId)}>
+          {toggle}
+        </button>
+        <button type="button" onClick={() => handleReset(intervalId)}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
